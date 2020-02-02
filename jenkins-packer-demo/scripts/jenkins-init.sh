077f4b03b5d3ae287444deec104e42bfd0e2b2d4
dCode0 <awani.dede@gmail.com>
2020-02-02 17:15:09 +0200

    "userdata"

--- jenkins-packer-demo/scripts/jenkins-init.sh
+++ jenkins-packer-demo/scripts/jenkins-init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo yum -y update
+# -get install jenkins
 
+sudo yum -y update
 sudo yum -y install java-1.8.0-openjdk
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-
 sudo yum -y install jenkins
-
 sudo service jenkins start
